58d8469a89e72e99d9e2c8902b088acca7447e08
ContratopDev <contratop@molecule.net>
2022-09-07 08:26:39 +0200

    implementacion para testeo de Git y Winget, entre otras updates

--- Far-Resolver.ps1
+++ Far-Resolver.ps1
@@ -6,8 +6,8 @@ $Host.UI.RawUI.WindowTitle = "Far Resolver Ver. $ver"
 
 
 # Parse Check #######################
-$OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
-$architectureproc = (Get-WmiObject -Class Win32_ComputerSystem).SystemType
+$script:OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+$script:architectureproc = (Get-WmiObject -Class Win32_ComputerSystem).SystemType
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Write-Host "You didn't run FAR-Resolver as an Administrator. FAR-Resolver will self elevate to run as an Administrator and continue."
     Start-Sleep 1
@@ -197,21 +197,148 @@ while($while1){
                         }
                     }
                     2{
-                        if(-not(get-command git)){
+                        if(-not(get-command git -ErrorAction SilentlyContinue)){
                             write-warning "No esta disponible Git, no se puede descargar la lista de plugins"
                         }
                         else{
                             write-host "Descargando repositorio..."
                             Set-Location ..
-                            git clone htts://github.com/contratop/Far-Resolver
+                            git clone "htts://github.com/contratop/Far-Resolver"
                             write-host "Descarga finalizada" -ForegroundColor Green
                         }
                     }
                     3{
-                        
+                        if(Get-Command winget -ErrorAction SilentlyContinue){
+                            write-host "Winget esta funcionando correctamente"
+                        }
+                        else{
+                            Write-Warning "No se ha detectado Winget"
+                            write-host "Se va a proceder a instalar Winget..."
+                            write-host ""
+                            Start-Sleep -s 2
+                            write-host "Arquitectura $architectureproc"
+                            write-host "Instalando marcos.."
+                            Start-Sleep -s 2
+                            if($architectureproc -match "64"){
+                                if(-not(test-path -path "marcos64.Appx")){
+                                    write-host "Descargando marcos64.Appx..."
+                                    Invoke-WebRequest -uri "https://github.com/contratop/Sources/raw/main/far_data/marcos64.Appx" -OutFile "marcos64.Appx"
+                                    if(-not($?)){
+                                        Write-Warning "Error en la descarga"
+                                        Write-host "Instalando Winget de todas formas"
+                                    }
+                                    else{
+                                        Add-AppPackage "marcos64.Appx"
+                                        if($?){
+                                            write-host "Marcos x64 instalados correctamente" -ForegroundColor Green
+                                        }
+                                        else{
+                                            Write-host "Error al instalar marcos" -ForegroundColor Yellow
+                                            write-host "Se intentara instalar Winget de todas formas"
+                                            Start-Sleep -s 5
+                                        }
+                                    }
+                                }
+                                elseif(Test-Path -path "marcos64.Appx"){
+                                    Add-AppPackage "marcos64.Appx"
+                                    if($?){
+                                        write-host "Marcos x64 instalados correctamente" -ForegroundColor Green
+                                    }
+                                    else{
+                                        Write-host "Error al instalar marcos" -ForegroundColor Yellow
+                                        write-host "Se intentara instalar Winget de todas formas"
+                                        Start-Sleep -s 5
+                                    }
+                                }
+                            }
+                            else{
+                                if(-not(test-path -path "marcos86.Appx")){
+                                    write-host "Descargando marcos86.Appx..."
+                                    Invoke-WebRequest -uri "https://github.com/contratop/Sources/raw/main/far_data/marcos86.Appx" -OutFile "marcos86.Appx"
+                                    if(-not($?)){
+                                        Write-Warning "Error al descargar"
+                                        Write-host "Instalando Winget de todas formas"
+                                    }
+                                    else{
+                                        Add-AppPackage "marcos86.Appx"
+                                        if($?){
+                                            write-host "Marcos x86 instalados correctamente" -ForegroundColor Green
+                                        }
+                                        else{
+                                            Write-host "Error al instalar marcos" -ForegroundColor Yellow
+                                            write-host "Se intentara instalar Winget de todas formas"
+                                            Start-Sleep -s 5
+                                        }
+                                    }
+                                }
+                                elseif(test-path -path "marcos86.Appx"){
+                                    Add-AppPackage "marcos86.Appx"
+                                    if($?){
+                                        write-host "Marcos x86 instalados correctamente" -ForegroundColor Green
+                                    }
+                                    else{
+                                        Write-host "Error al instalar marcos" -ForegroundColor Yellow
+                                        write-host "Se intentara instalar Winget de todas formas"
+                                        Start-Sleep -s 5
+                                    }
+                                }
+                            }
+                            write-host ""
+                            if(-not(test-path -path "winget.Msixbundle")){
+                                write-host "Descargando Winget..."
+                                Invoke-WebRequest -uri "https://github.com/contratop/Sources/raw/main/far_data/winget.Msixbundle" -OutFile "winget.Msixbundle"
+                                if($?){
+                                    Write-Host "Instalando Winget..."
+                                    Add-AppPackage "winget.Msixbundle"
+                                    if($?){
+                                        Write-host "Instalacion de Winget completada correctamente" -ForegroundColor Green
+                                    }
+                                    else{
+                                        Write-Warning "La instalacion de winget ha fallado"
+                                    }
+                                }
+                                else{
+                                    Write-Warning "Error al descargar Winget"
+                                }
+                            }
+                            elseif(test-path -path "winget.Msixbundle"){
+                                Write-Host "Instalando Winget..."
+                                Add-AppPackage "winget.Msixbundle"
+                                if($?){
+                                    Write-host "Instalacion de Winget completada correctamente" -ForegroundColor Green
+                                }
+                                else{
+                                    Write-Warning "La instalacion de winget ha fallado"
+                                }
+                            }
+                        }
+                        ""
+                        pause
                     }
                     4{
+                        if(Get-Command git -ErrorAction SilentlyContinue){
+                            Write-host "Winget ya esta instalado en el equipo" -ForegroundColor Green
+                        }
+                        elseif(Get-Command winget -ErrorAction SilentlyContinue){
+                            write-host "Instalando Git..."
+                            winget install git.git
+                            if(-not($?)){
+                                Write-Warning "Error al instalar Git"
+                            }
+                            else{
+                                Write-host "Instalacion de Git completada correctamente" -ForegroundColor Green
+                            }
+                        }
+                        elseif(-not(Get-Command winget -ErrorAction SilentlyContinue)){
+                            Write-Warning "No se puede instalar Git"
+                            Write-host "Winget no esta disponible"
+                        }
+                        else{
+                            Write-Warning "Error no especificado (Desbordamiento)"
+                        }
 
+                        ""
+                        pause
                     }
                     x{
                         $while2 = $false
